9fa50d0a5805b6da7b654cfb325e5bcc54087c80
wakoya-tech <waaqoyyaadhaabaa@gmail.com>
2025-10-06 15:06:37 +0300

    Add MyComponent.jsx

---
+++ .history/src/App_20251006111252.jsx
@@ -0,0 +1,10 @@
+
+import CarList from './CarList.jsx';
+
+function App() {
+  
+  return (
+<ToDoList/>
+  );
+}
+export default App;
---
+++ .history/src/ToDoList_20251006113900.jsx
@@ -0,0 +1,32 @@
+import React,{useState} from "react";
+function ToDoList(){
+    const [tasks,setTasks]=useState(["Eat Breakfast","Take a Shower","theater"]);
+
+    const [newTask,setNewTask]=useState("");
+ function handleInputChange(event){
+setNewTask(event.target.value)
+ }
+    function AddTask(){
+
+ }
+ function handleRemoved(index){
+
+ }
+ function handleMoveUp(){
+
+ }
+ function handleMoveDown(index){
+
+ }
+ 
+return(<div className="to-do-list">
+    <h2>To-Do-List</h2>
+   <input type="text"
+   placeholder="Enter a task..."
+   value={newTask}
+   onChange={handleInputChange} />
+   <button className="add-button"
+   onClick={AddTask}>Add</button>
+    </div>);
+}
+export default ToDoList
