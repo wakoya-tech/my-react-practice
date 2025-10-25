f9336e0832a85e20181306b4dcd91af0961e71aa
Wakoya Daba <waaqoyyaadhaabaa@gmail.com>
2025-10-25 10:23:58 +0300

    Update ProfilePicture.jsx

--- src/ProfilePicture.jsx
+++ src/ProfilePicture.jsx
@@ -5,7 +5,6 @@ function ProfilePicture() {
   const handleClick = (e) => {
     e.target.style.display = "none";
   };
-
   return (
     <>
       <img onClick={(e)=>handleClick(e)} src={image} alt="profile" />
