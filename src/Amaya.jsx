b79b8d5e145759e8d84c808c0348793903bee097
Wakoya Daba <waaqoyyaadhaabaa@gmail.com>
2025-10-25 10:19:16 +0300

    Update Amaya.jsx

--- src/Amaya.jsx
+++ src/Amaya.jsx
@@ -15,7 +15,6 @@ function Amaya(){
     function colorHandle(){
         setColor(c=>c === "green" ?"red":"green")
     }
-
     return(<>
     <p style={{color:color}}>Count:{count}</p>
     <button onClick={Add}>Add</button>
