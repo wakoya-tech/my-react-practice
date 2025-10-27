60bf02eb78f5f8c2921917768fb0b60bde63fce5
Wakoya Daba <waaqoyyaadhaabaa@gmail.com>
2025-10-27 22:56:48 +0300

    Update Button.jsx

--- src/Button.jsx
+++ src/Button.jsx
@@ -4,8 +4,6 @@ const handleClick1 = (e)=>e.target.textContent="Anaadhufu";
 return(<>
 <button onClick={(e)=>handleClick(e)}
 onDoubleClick={handleClick1}>click me</button>
-
-  
 </>)
 }
 export default Button
