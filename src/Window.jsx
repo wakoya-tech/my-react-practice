53163d1347a1f86edd6f9b362a76bdf7c5a613f5
Wakoya Daba <waaqoyyaadhaabaa@gmail.com>
2025-10-27 22:56:12 +0300

    Update Window.jsx

--- src/Window.jsx
+++ src/Window.jsx
@@ -2,7 +2,6 @@ import React,{useState,useEffect} from "react";
 function Window(){
     const [width,setWidth]=useState(window.innerWidth);
     const [height,setHeight]=useState(window.innerHeight);
-
     return(<>
     <p>window width :{width}px</p>
     <p>window height:{height}px</p>
