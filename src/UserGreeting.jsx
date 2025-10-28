48f24520d23f0284546f7a47122c3c168bb260f7
Wakoya Daba <waaqoyyaadhaabaa@gmail.com>
2025-10-28 22:52:32 +0300

    Update UserGreeting.jsx

--- src/UserGreeting.jsx
+++ src/UserGreeting.jsx
@@ -1,6 +1,6 @@
 function UserGreeting(props)
-{
-      return (props.isLoggedIn ? <h2>welcome {props.username}</h2> : <h2>please log in to continue</h2>);
+  {
+   return (props.isLoggedIn ? <h2>welcome {props.username}</h2> : <h2>please log in to continue</h2>);
   }
   export default UserGreeting;
   
