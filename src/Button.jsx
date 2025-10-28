a669fe49feb14859f697e2865da6e4d85efe94c1
Wakoya Daba <waaqoyyaadhaabaa@gmail.com>
2025-10-28 22:47:48 +0300

    Update button click handlers for text changes

--- src/Button.jsx
+++ src/Button.jsx
@@ -3,6 +3,7 @@ const handleClick = (e)=>e.target.textContent="welcome";
 const handleClick1 = (e)=>e.target.textContent="Anaadhufu";
 return(<>
 <button onClick={(e)=>handleClick(e)}
+  
 onDoubleClick={handleClick1}>click me</button>
 </>)
 }
