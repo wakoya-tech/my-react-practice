622e3a8252d67a8bb5ad46b047ace473d4f703fb
Wakoya Daba <waaqoyyaadhaabaa@gmail.com>
2026-03-05 20:20:00 +0300

    Update

--- src/Button.jsx
+++ src/Button.jsx
@@ -2,7 +2,6 @@ function Button(){
 const handleClick = (e)=>e.target.textContent="welcome";
 const handleClick1 = (e)=>e.target.textContent="Anaadhufu";
 return(<>
-
 <button onClick={(e)=>handleClick(e)}
 onDoubleClick={handleClick1}>click me</button>
 </>)
