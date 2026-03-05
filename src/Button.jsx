4c1a69cca2253967e1f157a236dbf820a3132335
Wakoya Daba <waaqoyyaadhaabaa@gmail.com>
2026-03-05 20:19:08 +0300

    Update Button component to handle click events

--- src/Button.jsx
+++ src/Button.jsx
@@ -2,6 +2,7 @@ function Button(){
 const handleClick = (e)=>e.target.textContent="welcome";
 const handleClick1 = (e)=>e.target.textContent="Anaadhufu";
 return(<>
+
   
 <button onClick={(e)=>handleClick(e)}
 onDoubleClick={handleClick1}>click me</button>
